5e1b36f9520354879f7918ae5e1b11913be010a4
Stas Dukach <stasdukach@gmail.com>
2021-06-02 10:31:57 +0300

    Updates

---
+++ precache-manifest.510efb52f83886dd1833d6be4edc19cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad4f87707b5e3ccda9b85cc786f8487",
+    "url": "./index.html"
+  },
+  {
+    "revision": "095fb2d204401941da01",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "569049a0f7a2898af4df",
+    "url": "./static/js/2.c53bfda9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c53bfda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095fb2d204401941da01",
+    "url": "./static/js/main.4a1c2096.chunk.js"
+  },
+  {
+    "revision": "cc2a5e26d5c4844758e0",
+    "url": "./static/js/runtime-main.beb1984a.js"
+  }
+]);
